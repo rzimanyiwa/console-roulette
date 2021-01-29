fdf6e446754f0e3c82ba638a4dfb6179512b54d1
Roy Zimanyiwa <rzimanyiwa@gmail.com>
2021-01-29 13:59:39 +0200

    Initial commit

---
+++ src/main/java/com/playsafeholdings/consoleroulette/ConsoleRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.playsafeholdings.consoleroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleRouletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleRouletteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playsafeholdings/consoleroulette/ConsoleRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafeholdings.consoleroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleRouletteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
