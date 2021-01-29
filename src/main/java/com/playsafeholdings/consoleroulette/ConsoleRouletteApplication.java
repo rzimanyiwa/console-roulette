fafa2b07c7e74d2cb8e128302f7daf10d17e5d04
Roy Zimanyiwa <rzimanyiwa@gmail.com>
2021-01-29 14:14:46 +0200

    Implemented reading from file on application startup

--- src/main/java/com/playsafeholdings/consoleroulette/ConsoleRouletteApplication.java
+++ src/main/java/com/playsafeholdings/consoleroulette/ConsoleRouletteApplication.java
@@ -1,13 +1,67 @@
 package com.playsafeholdings.consoleroulette;
 
+import com.playsafeholdings.consoleroulette.player.Player;
+import com.playsafeholdings.consoleroulette.player.PlayerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
-public class ConsoleRouletteApplication {
+public class ConsoleRouletteApplication implements CommandLineRunner {
+
+    private static Logger logger = LoggerFactory.getLogger(ConsoleRouletteApplication.class.getName());
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private PlayerService playerService;
 
     public static void main(String[] args) {
         SpringApplication.run(ConsoleRouletteApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception
+    {
+        fileToInputStream();
+    }
+
+    private String readFromInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    private void fileToInputStream() throws IOException {
+        String fileName = environment.getRequiredProperty("input.file.name");
+        InputStream inputStream = new ClassPathResource(fileName).getInputStream();
+        String[] players = readFromInputStream(inputStream).split("\\n");
+        for (String player : players) {
+            List<String> playerParams = Arrays.asList(player.split("\\,"));
+            Player newPlayer = new Player();
+            newPlayer.setName(playerParams.get(0));
+            newPlayer.setTotalBet(new BigDecimal(playerParams.get(1)));
+            newPlayer.setTotalWin(new BigDecimal(playerParams.get(2)));
+            playerService.addPlayer(newPlayer);
+        }
+    }
 }
